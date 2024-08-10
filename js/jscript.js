f809d4fb6b6f3e2f13cbdbc5ee76b2827f939248
Maath Mphepo <maathmphepo80@google.com>
2024-08-10 09:35:52 +0200

    separated js

--- js/jscript.js
+++ js/jscript.js
@@ -1,12 +1,42 @@
 function scrollDiv(direction) {
     const scrollableDiv = document.getElementById('scrollable-div');
-
-    // Define the amount to scroll each time
-    const scrollAmount = 50;
+    const scrollAmount = 500;
 
     if (direction === 'left') {
-        scrollableDiv.scrollLeft -= scrollAmount;
+        scrollableDiv.scrollTo({
+            left: scrollableDiv.scrollLeft - scrollAmount,
+            behavior: 'smooth'
+        });
     } else if (direction === 'right') {
-        scrollableDiv.scrollLeft += scrollAmount;
+        scrollableDiv.scrollTo({
+            left: scrollableDiv.scrollLeft + scrollAmount,
+            behavior: 'smooth'
+        });
     }
 }
+
+function toggleDiv() {
+    var div = document.getElementById("offcanvasNavbar");
+    if (div.classList.contains("show")) {
+      div.classList.remove("show");
+      setTimeout(() => { div.style.display = "none"; }, 300); // Wait for transition to complete
+    } else {
+      div.style.display = "flex";
+      setTimeout(() => { div.classList.add("show"); }, 10); // Allow display to update before transition
+    }
+  }
+  window.addEventListener('resize', () => {
+    var div = document.getElementById("offcanvasNavbar");
+    if (window.innerWidth >= 992) {
+      div.classList.remove("show");
+      div.style.display = "none";
+    }
+  });
+  document.addEventListener('click', function (event) {
+    var div = document.getElementById("offcanvasNavbar");
+    var button = document.querySelector('button');
+    if (div.classList.contains('show') && !div.contains(event.target) && !button.contains(event.target)) {
+      div.classList.remove("show");
+      setTimeout(() => { div.style.display = "none"; }, 300); // Wait for transition to complete
+    }
+  });
